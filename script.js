2d7e67197cd21a25ed5cf5617534ebf2c1ddb77c
LorenzoMinotti-090 <minottilorenzo@gmail.com>
2025-10-27 17:22:05 +0100

    fix: ragionamento

---
+++ script.js
@@ -0,0 +1,18 @@
+/*Raccolta dati
+
+i km da percorrere (km)
+l’età del passeggero (eta)
+*/
+
+/*Esecuzione logica
+
+Prezzo base = km × 0.21 €
+Se l’età è < 18 → sconto 20%
+Se l’età è > 65 → sconto 40%
+*/
+
+/* Output 
+
+Il prezzo finale viene mostrato con due decimali.
+Viene stampato sia in un alert che in console.log
+*/
