039524467e4a77b030a320495b528c8c86e0adfc
LorenzoMinotti-090 <minottilorenzo@gmail.com>
2025-10-27 17:34:51 +0100

    feat : pratica javascript

--- script.js
+++ script.js
@@ -16,3 +16,36 @@ Se l’età è > 65 → sconto 40%
 Il prezzo finale viene mostrato con due decimali.
 Viene stampato sia in un alert che in console.log
 */
+
+
+
+// --- RACCOLTA DATI ---
+
+
+const km = parseFloat(prompt("Quanti chilometri vuoi percorrere?"));
+const eta = parseInt(prompt("Quanti anni ha il passeggero?"));
+
+
+// --- ESECUZIONE LOGICA ---
+
+
+const prezzoKm = 0.21;
+let prezzoTotale = km * prezzoKm;
+
+if (eta < 18) {
+  prezzoTotale *= 0.8;
+  
+} 
+
+else if (eta > 65) {
+  prezzoTotale *= 0.6;
+}
+
+
+// --- OUTPUT ---
+
+
+const prezzoFinale = prezzoTotale.toFixed(2);
+
+console.log(`Il prezzo totale del biglietto è: € ${prezzoFinale}`);
+alert(`Il prezzo totale del biglietto è: € ${prezzoFinale}`);
